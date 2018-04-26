97c84b13dc353f88553bae26f948efab4eb35726
jack-buffington <other@buffingtonfx.com>
2018-04-26 17:53:17 -0400

    Mostly working python version

---
+++ PythonStuff/ICP06.py
@@ -0,0 +1,193 @@
+import numpy as np
+from convertScanToXY import convertScanToXY
+from scipy.spatial.distance import pdist,cdist
+import operator
+import pdb
+import math
+import time
+
+# def distance(x1,y1,x2,y2):
+#     dist=math.sqrt(((float(x1)-float(x2))*(float(x1)-float(x2)))+((float(y1)-float(y2))*(float(y1)-float(y2))))
+#     return dist
+
+
+
+def closestPoints(old,new):
+    distances = cdist(old,new,'euclidean')
+    # distances is a matrix where rows correspond to points in the 'old' matrix and 
+    # cols correspond to points in the 'new' matrix.
+    indices = np.argmin(distances, axis = 0)
+    closest = old[indices] 
+    return closest
+
+
+
+
+
+
+# CHECKED
+def doOneIteration(XY1,XY2):
+#   Find the closest point in XY2 for each point in XY1
+    closestPts=closestPoints(XY1,XY2)
+
+    #pdb.set_trace()
+    
+#   Find the covariance between the 2 matrices
+    cov=np.matmul((np.transpose(XY2)),closestPts)
+    
+    
+#   Use that to find the rotation
+    U, s, V = np.linalg.svd(cov, full_matrices=True)
+    rotationMatrix = np.matmul(V,U.T)
+
+#   Find the optimal translation
+    XY2 = np.matmul(XY2, rotationMatrix.T)
+
+#   Find their average translation from their corresponding closest points
+    diffs=XY2-closestPts
+    offset=np.mean(diffs,axis=0)
+    translation=-offset
+    
+#   Calculate the error in alignment as the sum of squared distances between point matches
+    err=math.sqrt((offset[0])*(offset[0])+(offset[1])*(offset[1]))
+
+    return rotationMatrix, translation, err
+
+
+
+
+
+
+# I'VE CHECKED THIS AS MUCH AS I CAN.
+def ICP06(XY1, XY2):
+
+    #start_time = time.time()
+
+    #########################################
+    # Interpolate the first scan as necessary
+    #########################################
+    # This is approximating point to plane ICP but instead of projecting onto the 
+    # normal, I am just creating more points in areas where things are sparse. 
+    numberOfPoints = XY1.shape[0]
+    C = np.zeros([2,2])  # this is necessary in Python to use pdist
+
+    maxDist = 50
+
+    for I in range(numberOfPoints - 2): 
+        A = XY1[I,:]
+        B = XY1[I+1,:] 
+
+        C[0,:] = A           # and is used to find the distance betwen them
+        C[1,:] = B
+        distance = pdist(C)
+
+        if distance > maxDist:
+            XY1 = interpolatePoints(A,B, distance, maxDist, XY1)
+
+
+    # Check the first and last points as a special case
+    A = XY1[numberOfPoints-1,:]
+    B = XY1[0,:]
+    C[0,:] = A
+    C[1,:] = B
+
+    distance = pdist(C)
+    if distance > maxDist:
+        XY1 = interpolatePoints(A,B, distance, maxDist, XY1)
+
+    #elapsed_time = time.time() - start_time
+
+
+
+    # ###############
+    # Do the ICP part
+    # ###############
+
+    translation, rotationMatrix = actualICP(XY1, XY2)
+
+    # rotate and translate the XY2 points
+    XY2temp = np.matmul(XY2,rotationMatrix.T)  # Resulting in a 154x1 matrix
+    XY2temp = XY2temp + translation;
+
+
+    # #########################################
+    # Remove points that don't align very well:
+    # #########################################
+
+
+    # * Find the distances to all of the closest points 
+    tempPoints = closestPoints(XY1,XY2temp)
+    minDistances = np.linalg.norm(XY2temp - tempPoints,axis = 1)
+
+    # * Figure out if there are any points that don't sit close to any points in the old
+    #     To find points that are outliers, look for points that are more than 1.5 times
+    #     the maxDist 
+
+    pointsToKeep = minDistances < maxDist * 1.5; # True and False in Python
+    count = np.array(range(len(pointsToKeep)))
+    pointsToKeep = pointsToKeep * count
+    pointsToKeep = pointsToKeep[pointsToKeep > 0] 
+
+    # and temporarily remove them from the new set.
+    XY2refined = XY2[pointsToKeep]
+
+
+
+    # ##############################################################
+    # Do ICP again with the two sets of points that match up better.
+    # ##############################################################
+
+    trans, rot = actualICP(XY1, XY2refined)
+
+
+    #transformMatrix = [rotationMatrix translation'; [0 0 1]] 
+    transformMatrix = np.matrix([[rot[0,0], rot[0,1], trans[0,0]],
+                                 [rot[1,0], rot[1,1], trans[0,1]],
+                                 [0,               0,         1]]);
+
+    return transformMatrix
+
+
+
+
+
+
+
+# THIS FUNCTION WAS CHECKED
+def interpolatePoints(A,B, distance, maxDistance, pointArray):
+    numberOfInterpolatedPoints = int(distance / maxDistance)
+    offset = (B - A)/float(numberOfInterpolatedPoints + 1)
+
+    currentLocation = A
+    for J in range(numberOfInterpolatedPoints):
+        currentLocation += offset
+        pointArray = np.append(pointArray,[currentLocation], axis = 0)
+    return pointArray
+
+
+
+
+
+# THIS FUNCTION WAS CHECKED
+def actualICP(XY1, XY2):
+    totalTranslation = np.matrix('0 0')
+    totalRotationMatrix = np.matrix('1 0; 0 1');
+    maxIterations = 50; 
+    errorThreshold = .00001;
+
+    for I in range (1,maxIterations):
+        rotationMatrix, translation, err = doOneIteration(XY1,XY2)
+
+        if err < errorThreshold:
+            break
+
+        XY2 = np.matmul(XY2,rotationMatrix.T)
+        XY2 = XY2 + translation
+
+        totalTranslation = np.matmul(totalTranslation,rotationMatrix.T) + translation 
+        totalRotationMatrix = np.matmul(rotationMatrix,totalRotationMatrix)
+
+    rotationMatrix = totalRotationMatrix;
+    translation = totalTranslation;
+
+    return [translation,rotationMatrix]
--- PythonStuff/scan2ScanMapping.py
+++ PythonStuff/scan2ScanMapping.py
@@ -1,52 +1,60 @@
 from dataFileParser import *
 from convertScanToXY import *
 from occupancyMap import *
-from ICP2 import * 
+from ICP06 import * 
 import time
 import numpy as np
 import pdb
 
-data = loadData('firstFloor.txt')
-#data = loadData('scanData.txt')
-occupancyMap = initMap();
-prevScan =  convertScanToXY(data[0]);
-occupancyMap = insertPoints(prevScan, occupancyMap);
-robotPos = np.array([0,0]);
-visualizeMap(occupancyMap,robotPos );
-#globalTrans = np.matrix([[0,0]]);
-#globalRot = np.matrix([[1,0],[0,1]]);
-globalTransform = np.matrix([[1,0,0],
-                             [0,1,0],
-                             [0,0,1]]); 
+fileData = loadData('firstFloor.txt')
+
+firstFrame = 90 - 1
+lastFrame = len(fileData) - 1
+
+occupancyMap = initMap()
+
+lastXY =  convertScanToXY(fileData[0])
+
+occupancyMap = insertPoints(lastXY, occupancyMap)
+
+robotPos = np.array([0,0])
+visualizeMap(occupancyMap,robotPos)
+
+cumulativeTransform = np.matrix([[1,0,0],
+                                 [0,1,0],
+                                 [0,0,1]]) 
 #pdb.set_trace();
 
-for i in range(80,200):
-    newScan = convertScanToXY(data[i]);
-    trans, rot = ICP2(prevScan, newScan);
-    intermediateTransform = np.matrix([ [rot[0,0], rot[0,1],trans[0,0]],
-                                        [rot[1,0], rot[1,1], trans[0,1]],
-                                        [0,0,1]]);
-    globalTransform = np.matmul(globalTransform,intermediateTransform);
+for i in range(firstFrame,lastFrame,5): 
+    XY = convertScanToXY(fileData[i])
+    # Strip away any 'points' that ended up being [0 0]
+    # TODO:  Check to see if this is necessary in Python.  Some points that aren't at 
+    # the end are also 0's 
+    temp = XY == 0 # This will be true and false
+    temp = temp * np.ones(temp.shape) # should now be 0's and 1's
+    temp = np.sum(temp, axis = 1) < 2 # should be true and false correspoinding
+                                      # to rows that we want to keep (True)
+    XY = XY[temp] # should be just coordinates that aren't [0 0]
+
+
+    thisTransform = ICP06(lastXY, XY)
+    lastXY = XY
+
+    cumulativeTransform = np.matmul(cumulativeTransform,thisTransform);
     
-    #globalTrans = globalTrans + (np.matmul(globalRot,trans.T)).T;
-    #globalRot = np.matmul(globalRot,rot);
-    updatedScan = np.append(newScan,np.ones([len(newScan),1]), axis=1);
-    #updatedScan = (np.matmul( globalRot,newScan.T)).T;
-    #updatedScan = updatedScan + globalTrans;
-    updatedScan = np.matmul( updatedScan, globalTransform.T); 
+    # Plot the transformed points
+    XY = np.append(XY,np.ones([len(XY),1]), axis=1)
+    XY = np.matmul( XY, cumulativeTransform.T)
+
+
+    occupancyMap = insertPoints(XY, occupancyMap)
+    robotPos = np.array([0,0]) 
+
+    # TODO:  Update the robot pose  DO THIS LATER... 
     
-    occupancyMap = insertPoints(updatedScan, occupancyMap);
-    robotPos = np.array([0,0]);
-    #robotPos = (np.matmul(globalRot, robotPos.T)).T;
-    #robotPos = globalTrans + robotPos; 
-    #robotPos = np.array([robotPos[0,0], robotPos[0,1]]); 
-    visualizeMap(occupancyMap, robotPos);
-    time.sleep(0.1);
-    prevScan = newScan; 
+    visualizeMap(occupancyMap, robotPos)
+    time.sleep(0.1)
+    
     
 
-'''for i in data:
-    scan = convertScanToXY(i);
-    occupancyMap = insertPoints(scan, occupancyMap);
-    visualizeMap(occupancyMap, np.array([0,0])); 
-    time.sleep(0.1);'''
+
